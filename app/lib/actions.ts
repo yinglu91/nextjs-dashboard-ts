22fec9f35c34114a72566f382d57f3a489621560
Ying Lu <yinglu5374@gmail.com>
2023-11-04 14:20:06 -0400

    add credential auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,24 @@
 'use server'
 
+import { signIn } from '@/auth'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { z } from 'zod'
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialSignin';
+    }
+    throw error;
+  }
+}
 
 const InvoiceSchema = z.object({
   id: z.string(),
