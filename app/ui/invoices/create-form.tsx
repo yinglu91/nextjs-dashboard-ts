985b0ef1ac4ad200dd723f3767809927bde176ca
Ying Lu <yinglu5374@gmail.com>
2023-11-04 03:11:41 -0400

    client side validation

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { CustomerField } from '@/app/lib/definitions'
 import Link from 'next/link'
 import {
@@ -71,6 +69,7 @@ export default function Form({ customers }: Props) {
                 step='0.01'
                 placeholder='Enter USD amount'
                 className='peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
+                required
               />
 
               <CurrencyDollarIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
